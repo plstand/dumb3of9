bec5b4cbf501677181a4678afaa4770f841b1b9e
PleaseStand <pleasestand@live.com>
2011-05-30 22:02:50 -0400

    Changed to using jQuery in test suite (decoding queue not yet implemented)

--- test-suite/test-suite.js
+++ test-suite/test-suite.js
@@ -1,42 +1,51 @@
 "use strict";
 
-var dec = Dumb3Of9Decoder,
-	ta = byId('ta'),
-	but = byId('decodeButton');
+$(function() {
+	var D39 = Dumb3Of9Decoder,
+		output = $('#output');
+		
+	function log(className, code) {
+		$('<li/>').prop('class', className).html(code).appendTo(output);
+	}
 
-function byId(id) {
-	return document.getElementById(id);
-}
-
-function showLine(str) {
-	ta.value += str;
-	ta.value += '\n';
-}
-
-but.onclick = function() {
-	var timeoutFired = false,
-		timeout = setTimeout(function() {
-			but.disabled = false;
-			but.innerHTML = but.oldHTML;
-			showLine('Image failed to load within 10 seconds.');
-		}, 10000),
-		startTime = Date.now();
-	but.disabled = true;
-	but.oldHTML = but.innerHTML;
-	but.innerHTML = 'Please wait...';
-	dec.loadImage(byId('filename').value, function(img) {
-		if(timeoutFired) {
+	$('#decodeButton').click(function() {
+		function timeoutHandler() {
+			timeoutFired = true;
+			log('error', 'The image failed to load.')
+		}
+		
+		var url = $('#filename').val();
+		
+		if(/^\s*$/.test(url)) {
+			log('error', 'You must fill in the filename field.');
 			return;
 		}
-		clearTimeout(timeout);
-		dec.decodeImageAsync(img, function(result) {
-			var endTime = Date.now();
-			if(result === null) {
-				result = '?';
+		
+		if(/^[^\/]*:/.test(url)) {
+			log('error', 'Because of browser security limitations, ' +
+				'this test suite cannot retrieve absolute URLs.');
+			return;
+		}
+		
+		var timeoutFired = false,
+			timeout = setTimeout(timeoutHandler, 10000),
+			startTime = $.now();
+		
+		D39.loadImage($('#filename').val(), function(img) {
+			if(timeoutFired) {
+				return;
 			}
-			showLine('(' + (endTime - startTime) + ' ms) ' + result);
-			but.disabled = false;
-			but.innerHTML = but.oldHTML;
+			clearTimeout(timeout);
+			D39.decodeImageAsync(img, function(result) {
+				var endTime = $.now();
+				if(result === null) {
+					log('error', 'No barcode recognized (took ' +
+						(endTime - startTime) + ' ms)');
+				} else {
+					log('ok', '<code>' + result + '</code> (took ' +
+						(endTime - startTime) + ' ms)');
+				}
+			});
 		});
 	});
-};
+});
